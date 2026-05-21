614450481f42c10f3e8bc34bd8ee46b9a45c5ecf
saints universe <105810988+cryptocrusader15@users.noreply.github.com>
2026-05-21 04:27:11 +0100

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ebook-cache-v1';
-const ASSETS = ['index.html', 'manifest.json', 'icon-512.png'];
+const CACHE_NAME = 'Prof Omego';
+const ASSETS = ['index.html', 'manifest.json', 'icon-512.png', 'UNIPORT-LOGO.png', 'prof.png', 'apple-icon.png'];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
